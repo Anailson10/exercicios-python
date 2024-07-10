85c0eaf8c510ab6af59e98767f66c9c0b5154ed7
Anailson10 <159083382+Anailson10@users.noreply.github.com>
2024-07-10 10:58:39 -0300

    Add files via upload

---
+++ automaçoes/pesquisar preço do dolar.py
@@ -0,0 +1,9 @@
+import pyautogui as pa
+import time 
+pa.PAUSE  = 1 
+
+pa.press('win')
+pa.write('chorme')
+pa.press('enter')
+pa.write('dolar hoje')
+pa.press('enter')
---
+++ automaçoes/youtube.py
@@ -0,0 +1,9 @@
+import pyautogui as pa
+import time
+pa.PAUSE = 1
+
+pa.press("win")
+pa.write("chorme")
+pa.press("enter")
+pa.write("youtube.com")
+pa.press("enter")
